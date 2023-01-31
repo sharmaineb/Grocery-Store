235d52408589d0a0dd690df08b5f612cc019afca
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-31 12:06:56 -0800

    [update] working on forms.py

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,16 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
-
     # TODO: Add the following fields to the form class:
     # - title - StringField
     # - address - StringField
     # - submit button
-    pass
+    title = StringField('Title', validators=[
+        DataRequired()
+    ])
+    address = StringField('Address', validators=[
+        DataRequired()
+    ])
+    submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
@@ -22,4 +27,17 @@ class GroceryItemForm(FlaskForm):
     # - photo_url - StringField
     # - store - QuerySelectField (specify the `query_factory` param)
     # - submit button
-    pass
+    name = StringField('Name', validators=[
+        DataRequired()
+    ])
+    price = FloatField('Price', validators=[
+        DataRequired()
+    ])
+    category = SelectField('Category', validators=[
+        DataRequired()
+    ])
+    photo_url = StringField('Photo URL', validators=[
+        DataRequired()
+    ])
+    store = QuerySelectField('Store', query_factory= ) # fix later
+    submit = SubmitField('Submit')
